2d9334049e6e1ae9ccc3d02e9463edf2a4544df1
Gaëlle Lannuzel <glannuzel@ensc.fr>
2022-03-18 12:31:48 +0100

    Describe right_arm.

--- src/kdl/models/reachy.cpp
+++ src/kdl/models/reachy.cpp
@@ -37,27 +37,52 @@ namespace KDL{
         // On ignore dans un premier temps le passage du repère reachy au r_shoulder_pitch
         // Les joints sont exprimés dans le repère du r_shoulder_pitch
         // PB de signe à régler, les z sont problablement orientés dans l'autre sens de temps en temps
+        // // r_shoulder_roll
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(0.0,PI_2,0.0,PI_2)));
+        // // r_arm_yaw
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(0.0,PI_2,0.0,-PI_2)));
+        // // r_elbow_pitch
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(0.0,-PI_2,0.28,PI_2)));
+        // // r_forearm_yaw
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(0.0,PI_2,0.0,0.0)));
+        // // r_wrist_pitch
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(0.0,-PI_2,0.25,0.0)));
+        // // r_wrist_roll
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(-0.0325,PI_2,0.0,PI_2)));
+        //  // r_gripper
+        // reachy.addSegment(Segment(Joint(Joint::RotZ),
+        //                            Frame::DH(-0.085,0,0.0,0)));
+
+        // TEST Reperes bien orientés
+        reachy.addSegment(Segment(Joint(Joint::None),
+                                   Frame::DH(0.0,PI_2,0.0,PI)));
         // r_shoulder_roll
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(0.0,PI_2,0.0,PI_2)));
+                                   Frame::DH(0.0,-PI_2,-0.19,PI_2)));
         // r_arm_yaw
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(0.0,PI_2,0.0,-PI_2)));
+                                   Frame::DH(0.0,-PI_2,0.0,-PI_2)));
         // r_elbow_pitch
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(0.0,-PI_2,0.28,PI_2)));
+                                   Frame::DH(0.0,PI_2,-0.28,PI_2)));
         // r_forearm_yaw
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(0.0,PI_2,0.0,0.0)));
+                                   Frame::DH(0.0,PI_2,0.0,PI)));
         // r_wrist_pitch
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(0.0,-PI_2,0.25,0.0)));
+                                   Frame::DH(0.0,-PI_2,-0.25,0.0)));
         // r_wrist_roll
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(-0.0325,PI_2,0.0,PI_2)));
-         // r_gripper
+                                   Frame::DH(-0.0325,-PI_2,-0.012,-PI_2)));
         reachy.addSegment(Segment(Joint(Joint::RotZ),
-                                   Frame::DH(-0.0325,PI_2,0.0,PI_2)));
+                                   Frame::DH(-0.085,0.0,0.0,0.0)));
+
         return reachy;
     }
 
